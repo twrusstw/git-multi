e2120f93c0f0315ebabf80c849b81bcbed97bee3
russliu <zx56978@gmail.com>
2023-10-22 20:33:09 +0800

    fix

---  install-git-multi.sh
+++  install-git-multi.sh
@@ -1,21 +1,21 @@
 ﻿#!/bin/bash
 
 # Define the installation directory
-INSTALL_DIR="/usr/local/bin/gitmulti"
+INSTALL_DIR="/usr/local/bin/git-multi"
 
 # Define the download URL for the gitmulti script
 GITMULTI_URL="https://gitlab.com/russliu/git-multi.git"
 
 mdkir -p $INSTALL_DIR
-echo "Downloading gitmulti.sh script..."
-curl -sSL $GITMULTI_URL -o $INSTALL_DIR/gitmulti.sh
-echo "Downloading gitmulti-completion.sh script..."
-curl -sSL $GITMULTI_URL -o $INSTALL_DIR/gitmulti-completion.sh
+echo "Downloading core.sh script..."
+curl -sSL $GITMULTI_URL -o $INSTALL_DIR/core.sh
+echo "Downloading auto-completion.sh script..."
+curl -sSL $GITMULTI_URL -o $INSTALL_DIR/auto-completion.sh
 
 # Make the script executable
 echo "Making gitmulti script executable..."
-chmod 777 $INSTALL_DIR/gitmulti.sh
-chmod 777 $INSTALL_DIR/gitmulti-completion.sh
+chmod 777 $INSTALL_DIR/core.sh
+chmod 777 $INSTALL_DIR/auto-completion.sh
 
 # Print installation complete message
 echo "gitmulti installation complete!"
@@ -24,13 +24,13 @@ echo "gitmulti installation complete!"
 # Append the code to the end of the .bashrc or .zshrc file
 {
   echo ""
-  "alias gitmulti="$INSTALL_DIR/gitmulti.sh""
+  "alias gitmulti="$INSTALL_DIR/core.sh""
   "# Add gitmulti completion"
-  "source /path/to/gitmulti-completion.sh"
+  "source /path/to/auto-completion.sh"
  } >> ~/.bashrc
 {
   echo ""
-  "alias gitmulti="$INSTALL_DIR/gitmulti.sh""
+  "alias gitmulti="$INSTALL_DIR/core.sh""
   "# Add gitmulti completion"
-  "source /path/to/gitmulti-completion.sh"
+  "source /path/to/auto-completion.sh"
  } >> ~/.zshrc
